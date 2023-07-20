fc54e6b3083fbc679fe36ffe74f3a288ddcbbec9
Filipe Reis <filipe.reis17@live.com>
2023-07-20 11:57:29 -0400

    Exercícios4
    Correção do ex016

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,25 +1,32 @@
-function verificar() {
-    var inicio = Number(document.getElementById('txtinicio').value);
-    var fim = Number(document.getElementById('txtfim').value);
-    var passo = Number(document.getElementById('txtpasso').value);
+function contar() {
+    let inicio = document.getElementById('txtinicio')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpasso')
+    let res = document.getElementById('res')   
 
-    var res = document.querySelector('div#res')
-    var msg = document.querySelector('div#msg')
-    var resultado = String('')
-    
-    if (fim == 0) {
-        msg.innerHTML = 'Impossível contar'
-        res.innerHTML = ''
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML =  'Impossível contar!' 
+        //window.alert('[ERRO] Faltam dados!')             
     } else {
-        if (passo == 0){
-            alert('Passo inválido! Considerando PASSO 1')
-            passo = 1
+        res.innerHTML = `Contando: <br>`
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
-        msg.innerHTML = 'Contando...'
-        for(var c = inicio; c <= fim; c += passo) {
-            resultado += c + ', ';
+        if (i <= f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
-        res.innerHTML = resultado.slice(0, -2);
-    }
-    
+        res.innerHTML += `\u{1F3C1}`        
+    }         
 }
