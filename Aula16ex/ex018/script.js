e21bdc96f4b49af3b04b7c50e4b5b9ce25c33227
Filipe Reis <filipe.reis17@live.com>
2023-07-23 17:44:02 -0400

    ex018_parte_1

---
+++ Aula16ex/ex018/script.js
@@ -0,0 +1,40 @@
+let num = document.querySelector('input#fnum')
+let tab = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        alert('Tudo OK')
+        /*let n = Number(num.value)
+        let vet = []
+        vet.push(n)
+        let item = document.createElement('option')
+        item.text = `Valor ${n} adicionado`
+        tab.appendChild(item)*/
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+}
+
+function finalizar(vet) {
+    vetAnalisador.push(vet)
+
+}
