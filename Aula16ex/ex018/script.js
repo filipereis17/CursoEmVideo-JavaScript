b4a15db945e5c028d301c1ddaa84f2807e563361
Filipe Reis <filipe.reis17@live.com>
2023-07-24 15:41:00 -0400

    ex018_function_finalizar

--- Aula16ex/ex018/script.js
+++ Aula16ex/ex018/script.js
@@ -20,19 +20,33 @@ function inLista(n, l) {
 }
 
 function adicionar() {
-    
+    res.innerHTML =  ''
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        let n = Number(num.value)
-        valores.push(n)
+        valores.push(Number(num.value))
         let item = document.createElement('option')
-        item.text = `Valor ${n} adicionado`
+        item.text = `Valor ${num.value} adicionado`
         tab.appendChild(item)
     } else {
         window.alert('Valor inválido ou já encontrado na lista')
     }
+    num.value = ''
+    num.focus()
 }
 
 function finalizar() {
-    valores
-
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalzar!')
+    } else {    
+        tam = valores.length
+        res.innerHTML = `Ao todo, temos ${tam} cadastrados<br><br>`
+        valores.sort()
+        res.innerHTML += `O maior valor informado foi ${valores[tam - 1]}<br><br>`
+        res.innerHTML += `O menor valor informado foi ${valores[0]}<br><br>`
+        let soma = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+        }
+        res.innerHTML += `Somando todos os valores, temos  ${soma}<br><br>`
+        res.innerHTML += `A média dos valores digitados é  ${soma/tam}<br><br>`
+    }
 }
